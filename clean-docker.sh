a17f830a79391cc359317f6a3b84cbf45dc6c8ff
icotd <icot.site@gmail.com>
2024-09-30 19:55:49 +0300

    Update clean-docker.sh

--- clean-docker.sh
+++ clean-docker.sh
@@ -6,11 +6,5 @@ docker stop $(docker ps -q)
 # Remove all containers
 docker rm $(docker ps -aq)
 
-# Optional: Remove unused images
-docker image prune -a
-
-# Optional: Remove unused volumes
-docker volume prune
-
-# Optional: Remove all unused resources
-docker system prune -a
+# Remove all unused resources
+docker system prune -a --volumes -f
