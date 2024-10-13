d1ea1e8bc16f90fb8d226fd060c4491e75207344
icotd <icot.site@gmail.com>
2024-10-13 08:57:03 +0300

    Update android-tools.sh

--- android-tools.sh
+++ android-tools.sh
@@ -13,6 +13,8 @@ Mac https://dl.google.com/android/repository/platform-tools-latest-darwin.zip
 Linux https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 Windows https://dl.google.com/android/repository/platform-tools-latest-windows.zip
 
+https://dl.google.com/android/repository/platform- .zip
+
 # Download and extract the contents
 curl --location -o android.zip $commandlinetoolsDownloadUrl
 unzip -q android.zip -d ./android-temp
@@ -21,3 +23,26 @@ mkdir -p "$destination/cmdline-tools/latest"
 mv ./android-temp/cmdline-tools/* "$destination/cmdline-tools/latest"
 rm -rf ./android-temp
 # rm android.zip
+
+#!/bin/bash
+
+# Example Definitions, for Mac Setups
+destination="$HOME/android/"
+OS=
+
+manual download sdk:
+
+https://dl.google.com/android/repository/platform-29_r04.zip
+https://dl.google.com/android/repository/3534162-studio.sdk-patcher.zip
+https://dl.google.com/android/repository/build-tools_r29.0.3-windows.zip
+https://dl.google.com/android/repository/emulator-windows-6306047.zip
+https://dl.google.com/android/repository/sources-29_r01.zip
+https://dl.google.com/android/repository/platform-tools_r29.0.6-windows.zip
+
+Unpack to
+destination/Android\Sdk\platforms\android-2
+destination/Android\Sdk\patcher\v4
+destination/Android\Sdk\build-tools\29.0.3
+destination/Android\Sdk\emulator
+destination/Android\Sdk\sources\android-29
+destination/Android\Sdk\platform-tools
