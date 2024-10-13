e819b60833b0ecf188e858cbc50cf3ef05b49b9d
icotd <icot.site@gmail.com>
2024-10-13 08:44:05 +0300

    Update android-tools.sh

--- android-tools.sh
+++ android-tools.sh
@@ -2,10 +2,19 @@
 
 # Example Definitions, for Mac Setups
 destination="$HOME/android/"
-toolsDownloadUrl=$(curl https://developer.android.com/studio | grep -o "https:\/\/dl.google.com\/android\/repository\/commandlinetools\-mac\-[0-9]*_latest\.zip")
+commandlinetoolsDownloadUrl=$(curl https://developer.android.com/studio | grep -o "https:\/\/dl.google.com\/android\/repository\/commandlinetools\-mac\-[0-9]*_latest\.zip")
+
+toolsDownloadUrl=$(curl https://developer.android.com/studio | grep -o "https://dl.google.com/android/repository/platform-tools-latest-darwin.zip")
+ # toolsDownloadUrl=$(curl https://developer.android.com/studio | grep -o "https://dl.google.com/android/repository/platforms-latest-darwin.zip")
+
+Download lastest Android platform tools
+
+Mac https://dl.google.com/android/repository/platform-tools-latest-darwin.zip
+Linux https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+Windows https://dl.google.com/android/repository/platform-tools-latest-windows.zip
 
 # Download and extract the contents
-curl --location -o android.zip $toolsDownloadUrl
+curl --location -o android.zip $commandlinetoolsDownloadUrl
 unzip -q android.zip -d ./android-temp
 
 mkdir -p "$destination/cmdline-tools/latest"
